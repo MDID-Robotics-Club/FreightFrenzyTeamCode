7a461945f18ff256999b8dd831f3a78a61b42741
Alb33rt <albert931023@gmail.com>
2022-01-22 17:33:06 +0800

    add mdidlib/GyroController

--- mdidlib/GyroController.java
+++ mdidlib/GyroController.java
@@ -1,4 +1,34 @@
 package org.firstinspires.ftc.teamcode.mdidlib;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+
 public class GyroController {
+
+    public BNO055IMU gyro;
+    public BNO055IMU.Parameters parameters;
+    double p;
+
+    public GyroController(BNO055IMU defaultGyro) {
+        gyro = defaultGyro;
+        parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        gyro.initialize(parameters);
+    }
+
+    public GyroController(ModernRoboticsI2cGyro I2cGyro) {
+
+    }
+
+    public void setPConstant(double kP) {
+        p = kP;
+    }
+
+    public double[] maintainEncoderDriveAngle(double referenceAngle, double stateAngle) {
+
+    }
+
+    public double[] turnEncoderTurnToAngle(double referenceAngle, double stateAngle) {
+
+    }
 }
